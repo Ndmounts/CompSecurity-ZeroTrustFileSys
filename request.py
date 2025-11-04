290b27acd22683988b214ea0fe3c3cef62b0fb6e
xander <xandergot@gmail.com>
2025-11-04 16:57:37 -0500

    added mkdir request handaling

--- request.py
+++ request.py
@@ -4,10 +4,10 @@ include os
 
 
 
-def whitelistCheck(user,filename):
+def whitelistCheck(user,filename,permissions):
     try:
         with open("whitelist.json", "r") as whitelist:
-            if user in jason.load(whitelist)[filename][read]:
+            if user in jason.load(whitelist)[filename][permissions]:
                 return 1
             else:
                 return 0
@@ -16,18 +16,23 @@ def whitelistCheck(user,filename):
         return -1
     
 def view_request(user,filename):
-    perm =  whitelistCheck(user,filename)
+    perm =  whitelistCheck(user,filename,"read")
     if perm == 0:
             print(user + " is not authorized to read " + filename)
     if perm == 1:
         fs_read(filename)
 
-def write_request(user,filename):
-    perm = whitelistCheck(user,filename,data)
+def write_request(user,filename,data):
+    perm = whitelistCheck(user,filename,"write")
     if perm == 0:
         print(user + " is not authorized to write to " + filename)
     if perm == 1:
         fs_write(filename,data)
 
-
+def mkdir_request(user,filename):
+    perm = whitelistCheck(user,filename,"mkdir")
+    if perm == 0:
+        print(user + " is not authorized to write to " + filename)
+    if perm == 1:
+        fs_mkdir(filename)
 
