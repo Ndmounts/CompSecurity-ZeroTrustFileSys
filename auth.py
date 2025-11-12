07084ef506f61a9c35d130f9e1e019dc404f62f5
xander <xandergot@gmail.com>
2025-11-12 18:33:52 -0500

    fixed a input type

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from cryptography.x509.oid import NameOID
 
 
 def user_from_cert(): # also verifies cert
-    leaf_cert_path = certs/curent.csr
+    leaf_cert_path = "certs/curent.csr"
     try:
         with open(leaf_cert_path, "rb") as f:
             leaf_cert_data = f.read()
