f8bdf485366cc5f27aa332d610a5686e961b6b53
xandergot <156118420+xandergot@users.noreply.github.com>
2025-11-16 15:29:03 +0000

    put into  workable state

--- auth.py
+++ auth.py
@@ -3,11 +3,13 @@ import fnmatch
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives.asymmetric import padding
+import traceback
 
 
 def user_from_cert(): # also verifies cert
-    leaf_cert_path = "certs/curent.csr"
-    root_ca_path = "certs/curent_root.csr"
+    leaf_cert_path = "certs/curent.crt"
+    root_ca_path = "certs/curent_root.crt"
     try:
         with open(leaf_cert_path, "rb") as f:
             leaf_cert_data = f.read()
@@ -17,19 +19,19 @@ def user_from_cert(): # also verifies cert
             root_ca_data = f.read()
             root_ca_cert = x509.load_pem_x509_certificate(root_ca_data, default_backend())
 
-        store = Store([root_ca_cert])
-        policy = PolicyBuilder().build_server_verifier(leaf_cert.subject)
-        verifier = store.get_server_verifier(policy)
-        
-        verifier.verify(leaf_cert, [])
+        root_ca_cert.public_key().verify(
+            leaf_cert.signature,
+            leaf_cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),  
+            leaf_cert.signature_hash_algorithm,
+        )
+
         return(leaf_cert_data.cn_attributes[0].value)
 
-    # except VerificationError as e:
-    #     print(f"Certificate verification failed: {e}")
-    #     return "-1"
 
     except Exception as e:
         print(f"An error occurred: {e}")
+        traceback.print_exc()
         return "-1"
 
 
