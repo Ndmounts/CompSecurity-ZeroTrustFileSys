bda90f0c43cc60804b43bb88a07faf548635e9df
xander <xandergot@gmail.com>
2025-11-21 16:33:49 -0500

    updated cert gen

--- generate-certs.sh
+++ generate-certs.sh
@@ -1,4 +1,4 @@
-
+mkdir certs
 
 cd certs
 
@@ -8,7 +8,7 @@ openssl genrsa -out ca.key 4096
 # CA cert (self-signed)
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 \
   -subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=CA/CN=ztfs-root" \
-  -out ca.crt
+  -out curent_root.crt
 
 # server key
 openssl genrsa -out server.key 2048
@@ -20,7 +20,7 @@ user_cert_gen()
 	local CN="$1"
 	openssl req -new -key server.key \
 	-subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=Server/CN=${CN}" \
-		-out server.csr
+		-out curent.csr
 
 
 	# sign with CA
