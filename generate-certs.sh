0a03cc6eb91f664d5caaf4914ee79121ab5f7a48
xander <xandergot@gmail.com>
2025-11-21 16:44:47 -0500

    fixed so that it can be used in the make build

--- generate-certs.sh
+++ generate-certs.sh
@@ -3,26 +3,29 @@ mkdir certs
 cd certs
 
 # CA key (keep private)
-openssl genrsa -out ca.key 4096
+openssl genrsa -out curent_root.key 2048
 
-# CA cert (self-signed)
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 \
-  -subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=CA/CN=ztfs-root" \
+# Root CA certificate (self-signed)
+openssl req -x509 -new -nodes -key curent_root.key -sha256 -days 3650 \
+  -subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=Server/CN=server" \
   -out curent_root.crt
 
-# server key
-openssl genrsa -out server.key 2048
+user_cert_gen() {
+	local CN="$1"
+
+    echo "Generating certificate for user: $CN"
+
+    # User key
+    openssl genrsa -out "${CN}.key" 2048
+
+    # User CSR
+    openssl req -new -key "${CN}.key" \
+      -subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=User/CN=${CN}" \
+      -out "${CN}.csr"
+
+    # Sign user cert with root CA
+    openssl x509 -req -in "${CN}.csr" -CA curent_root.crt -CAkey curent_root.key \
+      -CAcreateserial -out "${CN}.crt" -days 825 -sha256
+}
 
 user_cert_gen "user1"
-
-user_cert_gen()
-	# csr
-	local CN="$1"
-	openssl req -new -key server.key \
-	-subj "/C=US/ST=CO/L=Denver/O=ZTFS/OU=Server/CN=${CN}" \
-		-out curent.csr
-
-
-	# sign with CA
-	openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial \
-		-out server.crt -days 825 -sha256
